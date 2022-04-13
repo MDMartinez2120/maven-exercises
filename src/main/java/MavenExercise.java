80ea358016a5684791ed66a6d520a1e7301237e3
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-13 16:07:37 -0500

    first commit

---
+++ src/main/java/MavenExercise.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class MavenExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String input = scanner.nextLine();
+        System.out.println("you entered: ");
+        System.out.println(input);
+        if (StringUtils.isNumeric(input)) {
+            System.out.println(input + "is a number!");
+        } else {
+            System.out.println(input + " is not a number!");
+        }
+        System.out.println("Flipped case: " + StringUtils.swapCase(input));
+        System.out.println("reversed " + StringUtils.reverse(input));
+    }
+}
